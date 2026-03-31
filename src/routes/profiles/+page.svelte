c7f0d2b14a9f708ffaeb6f512aab4d60b148423c
Eli T. Drumm <eli@eli.td>
2026-03-30 21:25:15 -0400

    change some profiles; font sizing

--- src/routes/profiles/+page.svelte
+++ src/routes/profiles/+page.svelte
@@ -6,13 +6,18 @@
   <!-- <li><a href="https://replit.com/@etd"><img src="/icons/replit.svg" alt="Replit" /> <span class="citation" data-cites="etd">@etd</span></a></li> -->
   <li><a href="https://discord.com/users/524413678456668163"><img src="/icons/discord.svg" alt="Discord" /> etdr</a></li>
   <li><a href="https://t.me/etdru"><img src="/icons/telegram.svg" alt="Telegram" /> <span class="citation" data-cites="etdru">@etdru</span></a></li>
-  <li><a href="https://open.spotify.com/user/dte.li"><img src="/icons/spotify.svg" alt="Spotify" /> dte.li</a></li>
-  <li><a href="https://www.last.fm/user/eliium"><img src="/icons/lastfm.svg" alt="last.fm" /> eliium</a></li>
+  
   <li><a href="https://www.tiktok.com/@eli.td"><img src="/icons/tiktok.svg" alt="TikTok" /> <span class="citation" data-cites="eli.td">@eli.td</span></a></li>
   <li><a href="https://www.twitch.tv/etdr"><img src="/icons/twitch.svg" alt="Twitch" /> etdr</a></li>
   <li><a href="https://steamcommunity.com/id/etdru"><img src="/icons/steam-circle.svg" alt="Steam" /> etdru</a></li>
-  <li><a href="https://twitter.com/esalalumbo"><img src="/icons/twitter.svg" alt="Twitter" /> <span class="citation" data-cites="esalalumbo">@esalalumbo</span></a></li>
+  <!-- <li><a href="https://twitter.com/esalalumbo"><img src="/icons/twitter.svg" alt="Twitter" /> <span class="citation" data-cites="esalalumbo">@esalalumbo</span></a></li> -->
   <li><a href="https://www.facebook.com/etdru/"><img src="/icons/facebook.svg" alt="Facebook" /> etdru</a></li>
+</ul>
+
+<h2>defunct</h2>
+<ul>
+  <li><a href="https://open.spotify.com/user/dte.li"><img src="/icons/spotify.svg" alt="Spotify" /> dte.li</a></li>
+  <li><a href="https://www.last.fm/user/eliium"><img src="/icons/lastfm.svg" alt="last.fm" /> eliium</a></li>
 </ul>
 
 
